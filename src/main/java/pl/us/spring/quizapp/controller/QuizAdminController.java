e51605113b151227e35e4dbc94af9227bd83b906
cs <siwon.cezary@gmail.com>
2023-03-24 10:18:49 +0100

    PatchQuiz

--- src/main/java/pl/us/spring/quizapp/controller/QuizAdminController.java
+++ src/main/java/pl/us/spring/quizapp/controller/QuizAdminController.java
@@ -1,10 +1,17 @@
 package pl.us.spring.quizapp.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.JsonPatchException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.us.spring.quizapp.model.Quiz;
 import pl.us.spring.quizapp.repository.QuizRepositoryInMemory;
+
+import java.util.Optional;
 
 @RestController
 public class QuizAdminController {
@@ -40,4 +47,48 @@ public class QuizAdminController {
                     : ResponseEntity.ok(update);
         }
     }
+
+    @PatchMapping(value = "/api/v1/admin/quizzes/{id}", consumes = "application/merge-patch+json")
+    public ResponseEntity<Quiz> pathQuiz(@PathVariable long id, @RequestBody JsonPatch pathedQuiz){
+        final Optional<Quiz> optionalQuiz = quizRepository.findById(id);
+        if (optionalQuiz.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        Quiz quiz = optionalQuiz.get();
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonQuiz = mapper.convertValue(quiz, JsonNode.class);
+        try {
+            final JsonNode applied = pathedQuiz.apply(jsonQuiz);
+            final Quiz actualQuiz = mapper.treeToValue(applied, Quiz.class);
+            quizRepository.update(actualQuiz);
+            return ResponseEntity.ok(actualQuiz);
+        } catch (JsonPatchException | JsonProcessingException e) {
+            return ResponseEntity.badRequest().build();
+        }
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
